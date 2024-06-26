9e3a5e01cbf26b72b59de3f996128e7e4918ec30
Ihab <ihab@utopios.net>
2024-06-26 11:45:44 +0200

    correction exercice 1

---
+++ Exercices-kubernetes/Exercice1/app-web/script.sh
@@ -0,0 +1,11 @@
+# Etape 1
+kind create cluster --name exercice1 --config resources/cluster-kind.yml
+
+# Etape 2
+docker build -t webapp ./app-web/src/.
+
+# Etape 3
+kind load docker-image webapp --name exercice1
+
+# Etape 4
+kubectl apply -f k8s/k8s.yml
